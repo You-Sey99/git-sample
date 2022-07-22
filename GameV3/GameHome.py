a458fa5ec8d163ce34f736c7760f5f983d2126ec
{0095nagasaki} <{bb38120095@ms.nagasaki-u.ac.jp}>
2022-07-22 16:00:18 +0900

    home_totyuu

--- GameV3/GameHome.py
+++ GameV3/GameHome.py
@@ -12,11 +12,26 @@ STORAGE_X = 40
 STORAGE_Y = 40
 STORAGE_ZURE_X = CARD_SIZE[0]*1.5
 STORAGE_ZURE_Y = CARD_ZURE_Y
+POSE_X = 100
+POSE_Y = 100
 
 class Home(lib.Scene):#ノーマルモードの管理クラス
-    def __init__(self, frame_size=5, bgc=BGC, surface=GAMENN):
-        super().__init__(frame_size, bgc, surface)
+    def __init__(self, frame_size=5, bgc=BGC, clock=30, surface=GAMENN):
+        super().__init__(frame_size, bgc, clock, surface)
+
         self.pose_bottun = lib.Bottun(txt="一時停止",rect=((POSE_X, POSE_Y),TBOX_SIZE))
     
     def back_ground(self,) -> None:
         super().back_ground()
+
+        self.pose_bottun.paint(Iro.SIRO)
+        self.pose_bottun.paint_txt(add_y=10)
+
+if __name__ == "__main__":
+    game = Home()
+    #for i in range(9):
+        #game.strgs[0].strg[i].set_no(10-i,)
+    res = ROOP_CODE
+    while res == ROOP_CODE:
+        res = game.main()
+        print("a")
